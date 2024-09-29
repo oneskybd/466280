f678295d9ee0bb8102039d4db434952e2a36c1a5
Omar Faruq <oneskybd9@gmail.com>
2024-09-29 15:48:51 +0600

    ShortHand Used

--- frontend/partials/slider.php
+++ frontend/partials/slider.php
@@ -1,6 +1,6 @@
 <div class="row bg-info">
         <div class="col">
-        <div id="carouselExampleIndicators" class="carousel    slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -9,16 +9,16 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img class="d-block w-100" src="<?php echo $img.'/1.jpg';?>" alt="First slide">
+                <img class="d-block w-100" src="<?= $img.'/1.jpg';?>" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo $img.'/2.jpg';?>" alt="Second slide">
+                <img class="d-block w-100" src="<?= $img.'/2.jpg';?>" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo $img.'/3.jpg';?>" alt="Third slide">
+                <img class="d-block w-100" src="<?= $img.'/3.jpg';?>" alt="Third slide">
                 </div>
                 <div class="carousel-item">
-                <img class="d-block w-100" src="<?php echo $img.'/4.jpg';?>" alt="Fourth slide">
+                <img class="d-block w-100" src="<?= $img.'/4.jpg';?>" alt="Fourth slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
